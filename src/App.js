49cff303ae5d2908050800862c8a3438323e2555
Duffey_kay <kayodesunday29@gmail.com>
2022-09-25 19:19:39 +0100

    bootstraped the project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-center font-bold text-blue-900'>Kay Araba</h1>
+    </div>
+  );
+}
+
+export default App;
